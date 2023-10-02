79452ceb597cd8d198c26890d697a057ffec07ce
ayeemartinez <ayemartinez8@gmail.com>
2023-10-02 14:58:54 -0300

    header change

--- app/components/EpisodesList/EpisodesList.tsx
+++ app/components/EpisodesList/EpisodesList.tsx
@@ -8,7 +8,7 @@ export const EpisodesList:FC <EpisodesList> = ({type}) => {
     return (
         <div className="w-full rounded-lg bg-neutrals-100  p-7 flex justify-center flex-wrap text-left lg:w-1/3">
             <div className="w-full text-left">
-                <h3 className="text-secondary-200 font-bold text-sm uppercase">{type} EPISODES</h3>
+                <h3 className="text-secondary-200 font-normal text-sm uppercase">{type} EPISODES</h3>
                 <div className="flex flex-wrap max-h-36 h-full overflow-y-scroll pt-3">
                     <span className="text-neutrals-400">S01E01 - Pilot - December 2, 2013</span>
                 </div>
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 
 export const Header = () => { 
     return (
-        <div className="text-center bg-neutrals-100 w-full py-6 p-5 md:p-7"> 
-           <h1 className="text-secondary-200 text-3xl font-semibold text-center w-full mb-4 uppercase font-mono">Welcome to my Rick and Morty app</h1> 
-           <span className="text-neutrals-400">Here you can see two lists of many Rick and Morty characters. You&apos;ll see more info about them if you pick one from each list.</span>
+        <div className="text-center bg-white w-full flex flex-wrap justify-center py-6 p-5 md:p-7 lg:p-11"> 
+           <h1 className="text-secondary-200 text-3xl font-medium text-center w-full mb-4 uppercase font-mono md:text-4xl">Welcome to my Rick and Morty&nbsp;app</h1> 
+            <span className="text-neutrals-400 tex-base md:text-lg">Here you can see two lists of many Rick and Morty characters. You&apos;ll see more info about them if you pick one from each list.</span>
         </div>
     )
 }
