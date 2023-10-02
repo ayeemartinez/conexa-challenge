66c1c6678e06641e7a4d5df87ad113a5dacae269
ayeemartinez <ayemartinez8@gmail.com>
2023-10-02 04:59:20 -0300

    misspelling

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Conexa Challenge',
   description: 'Conexa Challenge by Ayelen Martinez',
+  
 }
 
 export default function RootLayout({
